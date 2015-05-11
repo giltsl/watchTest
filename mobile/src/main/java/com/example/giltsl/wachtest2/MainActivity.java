6b1a23375b0e7203cc75c20a76cdf943cb1b0432
Gil Tselenchuk <giltsl@gmail.com>
2015-05-11 09:36:20 +0300

    connect button added to mobile

--- mobile/src/main/java/com/example/giltsl/wachtest2/MainActivity.java
+++ mobile/src/main/java/com/example/giltsl/wachtest2/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -12,23 +15,90 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.CapabilityApi;
 import com.google.android.gms.wearable.CapabilityInfo;
 import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.Wearable;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Set;
 
 
 public class MainActivity extends ActionBarActivity {
 
     final String TAG = getClass().getName();
+    private GoogleApiClient gApiClient;
+    TextView textView;
+    Button connectButton;
+    private boolean connected = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.text);
+        connectButton = (Button)findViewById(R.id.connectButton);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!connected) {
+                    connect();
+                }
+            }
+        });
+
+
 
     }
+
+    private void connect() {
+
+        gApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        Log.d(TAG, "onConnected bundle = " + bundle.toString());
+
+                        connected = true;
+
+                        Wearable.MessageApi.addListener(gApiClient, new MessageApi.MessageListener() {
+                            @Override
+                            public void onMessageReceived(MessageEvent messageEvent) {
+
+                                byte[] bytesData = messageEvent.getData();
+                                try {
+                                    String decoded = new String(bytesData, "UTF-8");
+                                    textView.setText(decoded);
+
+                                } catch (UnsupportedEncodingException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        Log.d(TAG, "onConnectionSuspended int = " + i );
+                        connected = false;
+
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+                        Log.d(TAG, "onConnectionFailed ConnectionResult  = " + connectionResult.toString());
+                        connected = false;
+
+                    }
+                })
+                .addApiIfAvailable(Wearable.API)
+                .build();
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
